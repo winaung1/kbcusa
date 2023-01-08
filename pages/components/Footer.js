6114e0368fd0af0e275355f45db2a9833dbbdd1e
Centernot <118785649+Centernot@users.noreply.github.com>
2023-01-07 23:13:31 -0500

    change

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -53,6 +53,11 @@ function Footer() {
       title: "တၢ်ဆဲးကျိး" ,
       link: "/components/SixHomePage/SixHomePage"
     },
+    {
+      id: 8,
+      title: "Water" ,
+      link: "/components/SixHomePage/SixHomePage"
+    },
   ];
   return (
     <div className='bg h-54 text-white'>
